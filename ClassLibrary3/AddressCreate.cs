ff0a48c0510ff996b43e6e43a35fb17988b1ef50
Erika <erikapeltz0@gmail.com>
2021-04-26 17:36:26 -0400

    Address Models

---
+++ ClassLibrary3/AddressCreate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardBrowser.Models
+{
+    public class AddressCreate
+    {
+      
+        [Required]
+        public string AddressLine1 { get; set; }
+        [Required]
+        public string AddressLine2 { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public int ZipCode { get; set; }
+    }
+}
